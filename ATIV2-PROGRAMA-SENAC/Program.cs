962e64943c84a48ed9e85022dcf0021cac956ab2
gabri <gabri@DESKTOP-I4U4N50>
2023-07-07 21:16:49 -0300

    Adicionar arquivos de projeto.

---
+++ ATIV2-PROGRAMA-SENAC/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Escolha um número inteiro:");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (numero > 0)
+        {
+            Console.WriteLine("Este número é positivo.");
+        }
+        else
+        {
+            Console.WriteLine("Este número não é positivo.");
+        }
+    }
+}
